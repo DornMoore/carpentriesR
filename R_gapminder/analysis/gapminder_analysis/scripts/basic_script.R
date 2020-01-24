d10308894d31809b7bf28366f47e747300b9e28f
Dorn Moore <dorn@savingcranes.org>
2020-01-24 08:53:18 -0600

    End of Day

--- R_gapminder/analysis/gapminder_analysis/scripts/basic_script.R
+++ R_gapminder/analysis/gapminder_analysis/scripts/basic_script.R
@@ -12,11 +12,45 @@ ggplot(data = gapminder,
   geom_point(alpha = 0.5) + scale_x_log10() + geom_smooth(method = "lm")
 
 
-
+#filter out a dataframe with just records from teh americas
 americas <-filter(gapminder,continent == "Americas")
 
+#Create a plot with custom labels
 myplot <- ggplot(data = americas, 
        mapping=aes(x=year, y=lifeExp, color=continent)) + 
   geom_line() + facet_wrap(~country) + 
   labs(x="Year", y="Life Expectancy", title = "Figure 1")
-ggsave(filename = "../../analysis/myplot.png",plot=myplot, width=12, height=10, dpi=300 )
+
+#save our plot as a pdf
+ggsave(filename = "../../analysis/myplot.pdf",plot=myplot, width=12, height=10, dpi=300 )
+
+# Add a new column with population in millions
+gapminder$pop_millions <- gapminder$pop/1000000
+
+#############
+#Functions
+
+#Temp converter
+far2kelvin <- function(temp){
+  kelvin <- ((temp - 32) * 5/9) + 273.15
+  return(kelvin)
+}
+far2kelvin(32)
+
+k2c <- function(k){
+  celcius <- k-273.15
+  return(celcius)
+}
+
+
+# FILTER/SUBSET #
+europeSelect <- gapminder %>% 
+  filter(continent == "Europe") %>%  #Filter 
+  select(year, country, gdpPercap) # Select
+
+
+gapminder_after_1990 <- gapminder %>%
+  filter(year>1990)
+
+#Write the CSV of the output
+write.csv(gapminder_after_1990, file="../../analysis/gapminder_after_1990.csv", row.names=FALSE)
